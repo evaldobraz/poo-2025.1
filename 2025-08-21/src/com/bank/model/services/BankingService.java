c1d4a57e088a7c70d1fe319a0d079625381bdfed
Evaldo Braz <evaldo.ormindo@outlook.com>
2025-08-21 23:58:50 -0300

    Fixed 'Not enough balance'

--- 2025-08-21/src/com/bank/model/services/BankingService.java
+++ 2025-08-21/src/com/bank/model/services/BankingService.java
@@ -10,7 +10,7 @@ public class BankingService {
     public void withdraw(double amount, Account account)
     {
         if(amount > account.getWithdrawLimit()) throw new WithdrawLimitException("The amount exceeds withdraw limit");
-        if(account.getBalance() <= 0) throw new InsufficientBalanceException("Not enough balance");
+        if(account.getBalance()-amount <= 0) throw new InsufficientBalanceException("Not enough balance");
         account.withdraw(amount);
     }
 }
